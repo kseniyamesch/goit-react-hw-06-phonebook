9ec5b47b8ee10db84f541691d5f0a255ddc8e50b
Kseniia Malyshenko <kseniyamesch0101@gmail.com>
2022-07-15 13:47:50 +0300

    Добавляет Redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,20 @@
 import { nanoid } from 'nanoid';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter';
 
+import {
+  addContact,
+  deleteContact,
+  changeFilter,
+} from '../redux/contactsSlice';
+
 export function App() {
-
-  const defaultContacts = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
-
-  const [contacts, setContacts] = useState(() => JSON.parse(window.localStorage.getItem('contacts')) ?? defaultContacts);
-
-  const [filter, setFilter] = useState('');
+  const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts.items);
+  const filter = useSelector(state => state.contacts.filter);
 
   const formSubmitHandler = data => {
     const contact = {
@@ -31,17 +30,16 @@ export function App() {
     ) {
       return alert(`${data.name} is already in Contact List`);
     }
-
-    setContacts(state => [...state, contact]);
+    dispatch(addContact(contact));
   };
 
   const deleteItem = id => {
-    setContacts(state => state.filter(contact => contact.id !== id));
+    dispatch(deleteContact(id));
   };
 
   const onFilter = evt => {
     const { value } = evt.currentTarget;
-    setFilter(value.trim);
+    dispatch(changeFilter(value));
   };
 
   const renderContact = () => {
@@ -49,11 +47,6 @@ export function App() {
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
-
-
-  useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
 
   return (
     <div>
